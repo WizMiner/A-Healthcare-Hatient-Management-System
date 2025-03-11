8ea2a97b9d3e5b5cbceabb53cad4c383b9e68a75
Minte_DEV <mintesinottamene0917@gmail.com>
2025-03-11 10:41:49 +0300

    fixing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import PatientForm from "@/components/forms/PatientForm";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+import { PatientForm } from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
@@ -21,7 +25,7 @@ export default function Home() {
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 Abrihot
+              © 2024 CarePluse
             </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
@@ -39,4 +43,6 @@ export default function Home() {
       />
     </div>
   );
-}
+};
+
+export default Home;
